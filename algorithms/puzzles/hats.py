e9585442e05b019ccf8b1fa0360f1d7bb87a2dba
hoodielive <hoodielive@gmail.com>
2018-12-12 19:49:28 -0500

    honies

---
+++ algorithms/puzzles/hats.py
@@ -0,0 +1,14 @@
+# When people are free to do as they please, they usually imitate each other.
+'''
+Let's say we have a whole bunch of fine females waiting in line to get into a concert.
+Some are extremely fine
+Some are not
+Those that are enter premium access
+Those that are not go to standard
+13 honies in total
+'''
+def premiumAccess(honies):
+    start = forward = backward = 0
+    intervals = []
+    for i in range(1, len(honies)):
+        if honies[start] != honies[i]
