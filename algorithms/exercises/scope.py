95754a6862b549a547691e55d74523e800fe9719
hoodielive <hoodielive@gmail.com>
2018-12-19 19:17:06 -0500

    scope

---
+++ algorithms/exercises/scope.py
@@ -0,0 +1,11 @@
+def illustrate_scope():
+    s_enclosing = 'E'
+    def display():
+        s_local = 'L'
+        print("Local --> {}".format(s_local))
+        print("Enclosing --> {}".format(s_enclosing))
+        print("Global --> {}".format(s_global))
+        print("Built-in --> {}".format(abs))
+    display()
+s_global = 'G'
+illustrate_scope():
