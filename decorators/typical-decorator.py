dac3aa5b93599d57148bca1b0eff542f6e7704e6
hoodielive <hoodielive@gmail.com>
2018-12-29 07:24:03 -0500

    Who ain even know it..

--- decorators/typical-decorator.py
+++ decorators/typical-decorator.py
@@ -1,5 +1,20 @@
+def mapper(func):
+    def inner(list_of_values):
+       return [func(value) for value in list_of_values] 
+    return inner
+
+@mapper
 def camelcase(s):
     """Turn strings_like_this into StringsLikeThis"""
     return ''.join([word.capitalize() for word in s.split('_')]) 
 
-print(camelcase('some_string'))
+#print(camelcase('some_string'))
+
+names = [
+        'rick_ross', 
+        'a$ap_rocky',
+        'kendrick_lamar',
+        'le$_steaknshrimp'
+]
+
+print(camelcase(names))
