4632e698e002ef06b890b6fde6deeea56d33e5c9
hoodielive <hoodielive@gmail.com>
2018-12-29 06:47:29 -0500

    work more with decorators

--- decorators/decorators.py
+++ decorators/decorators.py
@@ -8,3 +8,20 @@ def add(x, y):
     """Return the sum of x and y."""
     return x + y 
 add = decorated_by(add)
+
+def also_decorated_by(func): 
+    pass
+
+# the above can be done better 
+@also_decorated_by
+@decorator_by
+def add(x, y):
+    """Return the sum of x and y."""
+    return x + y 
+
+# the above is syntactically equivalent to: add = also_decorated_by(decorated_by(add))
+# the function call will be from botton to top
+# the add function will execute normally, then the decorated_by being sent to the decorated method 
+# decorated_by will return its own callable (in this case a modified add)
+# the returned value from decorated_by will be sent to also_decorated_by
+
