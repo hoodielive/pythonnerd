35edee92bcff10d31521bbd1105fd92a8bb894d5
hoodielive <hoodielive@gmail.com>
2019-01-02 13:48:14 -0500

    static methods and class methods

--- complete/oopp/static-methods.py
+++ complete/oopp/static-methods.py
@@ -6,3 +6,25 @@ class Student:
 
     def average(self): 
         return sum(self.marks) / len(self.marks)
+
+larry = Student('Larry', 'CMU')
+larry.marks.append(95)
+larry.marks.append(99)
+
+print(larry.average()) # or print(Student.average(larry))
+
+class Foo:
+    @classmethod 
+    def hi(cls):
+        print(cls.__name__)
+
+my_object = Foo() 
+my_object.hi() 
+
+class Bar:
+    @staticmethod
+    def hi():
+        print("Hello, I don't take parameters")
+
+another_object = Bar() 
+another_object.hi() 
