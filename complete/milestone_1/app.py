93cbf4bdf9cd99610d12a1d743ce379d8fbed883
hoodielive <hoodielive@gmail.com>
2019-01-02 08:38:12 -0500

    da fuhhhhh did I just do lol

--- complete/milestone_1/app.py
+++ complete/milestone_1/app.py
@@ -8,9 +8,9 @@
 Tasks:
 [x]: Decide where to store movies
 [x]: Show the user the main interface and get their input
-[]: Allow users to add movies
-[]: Show all their movies
-[]: Find a movie
+[x]: Allow users to add movies
+[x]: Show all their movies
+[x]: Find a movie
 [x]: Stop running the program when they type 'q'
 """
 
@@ -32,14 +32,12 @@ def menu():
         if user_input == 'a':
             add_movie()
         elif user_input == 'l':
-            show_movies()
+            show_movies(movies)
         elif user_input == 'f':
             find_movie()
         else:
             print("Unknown command-please try again.")
-
-        user_input = input("Enter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie, & 'q' to quit: ")
-
+user_input = input("Enter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie, & 'q' to quit: ")
 
 def add_movie():
    name = input('Enter the movie name: ')
@@ -51,5 +49,34 @@ def add_movie():
        'director': director,
        'year': year
    })
+
+def show_movies(movies_list):
+    for movie in movies_list:
+        show_movie_details(movie)
+
+def show_movie_details(movie):
+    print(f"Name: {movie['name']}")
+    print(f"Director: {movie['director']}")
+    print(f"Release year: {movie['year']}")
+
+def find_movie():
+   find_by = input("What property of the movie are you looking for? ")
+   looking_for = input("What are you searching for? ")
+
+   found_movies = find_by_attributes(movies, looking_for, lambda x: x[find_by])
+
+   show_movies(found_movies)
+
+
+def find_by_attributes(items, expected, finder):
+    found = []
+
+    for i in items:
+        #if movie[find_by] == looking_for:
+        if finder(i) == expected:
+            found.append(i)
+
+    return found
+
 
 menu()
