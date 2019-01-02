e13885dd1184b6ad9a46aa1cbc3c6fd0766458fb
hoodielive <hoodielive@gmail.com>
2019-01-02 08:06:23 -0500

    add movie function

--- complete/milestone_1/app.py
+++ complete/milestone_1/app.py
@@ -6,14 +6,24 @@
 - stop running the program
 
 Tasks:
-[]: Decide where to store movies
-[]: Show the user the main interface and get their input
+[x]: Decide where to store movies
+[x]: Show the user the main interface and get their input
 []: Allow users to add movies
 []: Show all their movies
 []: Find a movie
-[]: Stop running the program when they type 'q'
+[x]: Stop running the program when they type 'q'
 """
 
+# Storage
+movies = []
+
+"""
+movie = {
+    'name': ... (str), 
+    'director': ... (str),
+    'year': ... (int) 
+}
+"""
 
 def menu():
     user_input = input("Enter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie, & 'q' to quit: ")
@@ -30,5 +40,16 @@ def menu():
 
         user_input = input("Enter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie, & 'q' to quit: ")
 
+
+def add_movie():
+   name = input('Enter the movie name: ')
+   director = input('Enter the movie director: ')
+   year = int(input('Enter the movie release year: '))
+
+   movies.append({
+       'name': name,
+       'director': director,
+       'year': year
+   })
 
 menu()
