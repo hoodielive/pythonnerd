bf65aa3bb2b3ba9f63ae6f04c23401bb2ed343a5
hoodielive <hoodielive@gmail.com>
2019-01-02 13:16:21 -0500

    magic methods are cool juice

---
+++ complete/oopp/magic-methods.py
@@ -0,0 +1,28 @@
+class Student:
+    def __init__(self, name):
+        self.name = name
+
+# Python calls the dunder method for your automatically
+movies = ['Matrix', 'Finding Nemo']
+print(movies.__class__) # list lol
+print("hi".__class__)
+print(len(movies)) 
+
+class Garage:
+    def __init__(self):
+        self.cars = []
+
+    def __len__(self):
+        return len(self.cars)
+    
+    def __getitem__(self, i):
+        return self.cars[i] 
+
+# Objects
+ford = Garage() 
+ford.cars.append('Fiesta')
+ford.cars.append('Focus')
+print(ford.cars)
+print(len(ford.cars))
+print(len(ford))
+print(ford[0])
