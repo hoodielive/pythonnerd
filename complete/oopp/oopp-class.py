590643728c55f86a6973fafcdf37ca6305d40ee8
hoodielive <hoodielive@gmail.com>
2019-01-02 12:47:34 -0500

    student class

---
+++ complete/oopp/oopp-class.py
@@ -0,0 +1,20 @@
+my_student = {
+        'name': 'Cool Dude', 
+        'grades': [70, 88, 90, 99], 
+        'average': None # something here - maybe a Symbol
+}
+
+class Student:
+    def __init__(self, new_name, new_grades):
+        self.name = new_name
+        self.grades = new_grades 
+
+    def average(self):
+        return sum(self.grades) / len(self.grades) 
+
+student_one = Student('hood', [70, 88, 90, 99])
+print(student_one.__class__)
+print(student_one.name)
+student_two = Student('Jose', [99, 98, 97, 96])
+print(student_two.name)
+
