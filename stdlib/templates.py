24de4d17bc4ce31a52a5e74fa63337cd0e28cce4
hoodielive <hoodielive@gmail.com>
2019-01-04 07:45:31 -0500

    string.Template and how character is escaped

--- stdlib/templates.py
+++ stdlib/templates.py
@@ -3,6 +3,7 @@ import string
 
 values = {'sing': 'lala'}
 
+# Note the trigger is escaped by repeating the character twice
 t = string.Template(""" 
     Variable            : $sing
     Escape              : $$ 
@@ -11,9 +12,19 @@ t = string.Template("""
 
 print('TEMPLATE:', t.substitute(values))
 
+# Note the trigger is escaped by repeating the character twice
 s = """
-Variable            : %(sing)s 
-Escape              : %%
-Variable in Text    : %(sing)siable
-"""
+    Variable            : %(sing)s 
+    Escape              : %%
+    Variable in text    : %(sing)siable
+    """
 print('INTERPOLATION:', s % values)
+
+# Note the trigger is escaped by repeating the character twice
+f = """
+    Variable            : {sing}
+    Escape              : {{}}
+    Variable in text    : {sing}iable
+    """
+
+print('FORMAT:', s.format(**values))
