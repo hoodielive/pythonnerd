cfa5f32a5d9b19da5710a52de9946f4653f163aa
hoodielive <hoodielive@gmail.com>
2019-01-07 23:17:51 -0500

    dynamic arrays and singly linked lists

---
+++ algorithms/array-lessons/dynamic-arrays.py
@@ -0,0 +1,20 @@
+x = [3, 2, 1, 10, 107]
+print(x)
+
+# segfault/out of range 
+#x[5] = 7
+
+# Estimate O(n) -> O(1) 
+
+O(1) + O(1) + O(n)
+
+2/3 O(1) + 1/3 O(n)
+
+n - logn / n O(1) + logn O(n)
+
+n = 8 
+
+8 - 3 / 8 (5/8) O(1) + 3/8 O(n)
+
+# ok lol
+
---
+++ algorithms/linked-lists/linked-lists.py
@@ -0,0 +1,4 @@
+# previous_node() | next_node() 
+#  (singly linked list) [3]->[4]->[10]->[12]->[11] 
+# [5] -> null 
+
