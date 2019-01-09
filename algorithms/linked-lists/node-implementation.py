588cf4f3ab86e5c3c62797cafc1aaffb725d6054
hoodielive <hoodielive@gmail.com>
2019-01-08 20:46:31 -0500

    add removal

--- algorithms/linked-lists/node-implementation.py
+++ algorithms/linked-lists/node-implementation.py
@@ -25,6 +25,24 @@ class LinkedList(object):
             # pointer - reference - head
             self.head = newNode
 
+    # Remove - remove the specified instance and change the pointer/reference
+    def remove(self, data):
+        if self.head is None:
+            return
+
+        self.size = self.size - 1
+        currentNode = self.head
+        previousNode = None
+
+        while currentNode.data != data:
+            previousNode = currentNode
+            currentNode = currentNode.nextNode
+
+        if previousNode is None:
+            self.head = currentNode.nextNode
+        else:
+            previousNode.nextNode = currentNode.nextNode
+
     # O(1)
     def size(self):
         return self.size
