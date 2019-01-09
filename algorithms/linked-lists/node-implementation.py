9519fb19354675cd44a65fa5514f77f798a1ed04
hoodielive <hoodielive@gmail.com>
2019-01-08 20:42:55 -0500

    add insertion and traversal of list

--- algorithms/linked-lists/node-implementation.py
+++ algorithms/linked-lists/node-implementation.py
@@ -39,3 +39,21 @@ class LinkedList(object):
             size += 1
             actualNode = actualNode.nextNode
         return size
+
+    # O(n) 
+    def insertEnd(self, data):
+        self.size = self.size + 1
+        newNode = Node(data)
+        actualNode = self.head
+        
+        while actualNode.nextNode is not None:
+            actualNode = actualNode.nextNode
+
+        actualNode.nextNode = newNode
+
+    def traverseList(self):
+        actualNode = self.head
+
+        while actualNode is not None:
+            print("%d" % actualNode.data)
+            actualNode = actualNode.nextNode
