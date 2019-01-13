d692e77931dbe332186c655ce1e9aae5354f9628
hoodielive <hoodielive@gmail.com>
2019-01-12 21:03:01 -0500

    just add cleanse.py

---
+++ complete/files/cleanse.py
@@ -0,0 +1,14 @@
+array = [50, 13, 15, 50]
+for nums in array:
+    if nums[0] > nums[1]:
+        if nums[1] < nums[2]:
+            if nums[j] == nums[3]:
+                print("hello")
+    elif nums[1] < nums[2]:
+        print('howdy')
+    else:
+        pass
+
+
+
+
