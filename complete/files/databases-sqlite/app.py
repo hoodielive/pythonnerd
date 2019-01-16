67d21b346b80750b46e2423c8f9fd57f558eb720
hoodielive <hoodielive@gmail.com>
2019-01-16 08:11:34 -0500

    added menu and basic logic to book organizer app

--- complete/files/databases-sqlite/app.py
+++ complete/files/databases-sqlite/app.py
@@ -11,10 +11,30 @@ Your choice:"""
 
 def menu():
     user_input = input(USER_CHOICE)
-    while user_input != 'q'
-        pass
+    while user_input != 'q':
+        if user_input == 'a':
+            prompt_add_book()
+        elif user_input == 'l':
+            list_books()
+        elif user_input == 'r':
+            prompt_read_book()
+        elif user_input == 'd':
+            prompt_delete_book()
+        else:
+            print("Unknown option selected. Please try again.")
+        user_input = input(USER_CHOICE)
 
 # def prompt_add_book() # ask for book name and author 
+def prompt_add_book():
+    name = input('Enter the new book name: ')
+    author = input('Enter the name of the author: ')
+    database.add_book(name, author)
+
+def list_books():
+    books = database.get_all_books()
+    for book in books:
+        print(book)
+
 # def list_books() # show all the books in our list
 # def prompt_read_book() # ask for a book name and change it to read in our list
 # def prompt_delete_book() # ask for book name and remove book from list
