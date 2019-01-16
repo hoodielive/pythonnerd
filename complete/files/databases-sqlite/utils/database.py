fd2119f45e4a77b5f3d253e1f1d9a4c2e8f160f3
hoodielive <hoodielive@gmail.com>
2019-01-16 08:13:53 -0500

    list_books function will simply return all books in memory: as a cursory placeholder until I feel like doing the sqlite database lol :)

--- complete/files/databases-sqlite/utils/database.py
+++ complete/files/databases-sqlite/utils/database.py
@@ -7,7 +7,7 @@ books = []
 def add_book(name, author):
     books.append({ 'name': name, 'author': author, 'read': False })
 
-# def prompt_add_book() # ask for book name and author 
-# def list_books() # show all the books in our list
-# def prompt_read_book() # ask for a book name and change it to read in our list
-# def prompt_delete_book() # ask for book name and remove book from list
+def get_all_books():
+    return books
+
+
