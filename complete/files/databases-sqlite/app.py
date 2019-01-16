b8025d6261da4110507c66a106a3c78730aefb48
hoodielive <hoodielive@gmail.com>
2019-01-16 09:00:32 -0500

    minor improvement on list_books() function part 2 lol

--- complete/files/databases-sqlite/app.py
+++ complete/files/databases-sqlite/app.py
@@ -34,6 +34,7 @@ def prompt_add_book():
 def list_books():
     books = database.get_all_books()
     for book in books:
+        read = 'YES' if book['read'] else 'NO'
         print(f"{book['name']} by {book['author']}, read: {book['read']}")
 
 # def prompt_read_book() # ask for a book name and change it to read in our list
