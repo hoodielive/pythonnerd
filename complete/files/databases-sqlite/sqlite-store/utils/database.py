bef2c0c274d88d2374847fca69041798d259416d
hoodielive <hoodielive@gmail.com>
2019-01-17 19:35:44 -0500

    finished update and delete functions

--- complete/files/databases-sqlite/sqlite-store/utils/database.py
+++ complete/files/databases-sqlite/sqlite-store/utils/database.py
@@ -20,7 +20,6 @@ def add_book(name, author):
     connection.commit()
     connection.close()
 
-
 def get_all_books():
     connection = sqlite3.connect('data.db')
     cursor = connection.cursor()
@@ -30,18 +29,18 @@ def get_all_books():
     return books
 
 def _save_all_books(books):
-    with open(books_file, 'w') as file:
-        json.dump(books, file)
+    pass
 
 def mark_book_as_read(name):
-    books = get_all_books()
-    for book in books:
-        if book['name'] == name:
-            book['read'] = True
-    _save_all_books(books)
-
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+    cursor.execute('UPDATE books SET read=1 WHERE name=?', (name,))
+    connection.commit()
+    connection.close()
 
 def delete_book(name):
-    books = get_all_books()
-    books = [book for book in books if book['name'] != name] 
-    _save_all_books(books)
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+    cursor.execute('DELETE FROM books WHERE name=?', (name,))
+    connection.commit()
+    connection.close()
