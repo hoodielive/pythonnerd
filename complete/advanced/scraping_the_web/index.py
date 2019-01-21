cc94ea269951ba2d8b60a8d442829dd5dd075a98
hoodielive <hoodielive@gmail.com>
2019-01-21 09:41:19 -0500

    lets just see, what we can scrape next lol

--- complete/advanced/scraping_the_web/index.py
+++ complete/advanced/scraping_the_web/index.py
@@ -2,51 +2,51 @@
 from bs4 import BeautifulSoup
 
 SIMPLE_HTML = '''<!doctype html>
-<html class="no-js" lang="">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Beautiful Soup</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
-        <!-- Place favicon.ico in the root directory -->
-
     </head>
     <body>
-        <!--[if lt IE 8]>
-            <p class="browserupgrade">
-            You are using an <strong>outdated</strong> browser. Please
-            <a href="http://browsehappy.com/">upgrade your browser</a> to improve
-            your experience.
-            </p>
-        <![endif]-->
         <h1>This is a title.</h1>
         <p class="subtitle">
-            Non sodales neque sodales ut etiam sit amet nisl purus? Elit, at imperdiet dui accumsan sit
-            amet nulla facilisi morbi tempus iaculis urna, id volutpat lacus laoreet non curabitur gravida!
+           Veganism makes me happy! But I'm not the type to preach to people about it.
+        </p>
+        <p>
+            Soda is a poison!
         </p>
         <ul>
-            <li>Klaus</li>
-            <li>Elijah</li>
-            <li>Marcellus</li>
-            <li>Rebekah</li>
+            <li>klaus</li>
+            <li>elijah</li>
+            <li>marcellus</li>
+            <li>rebekah</li>
         </ul>
     </body>
 </html>'''
 
 
 simple_soup = BeautifulSoup(SIMPLE_HTML, 'html.parser')
-
+print(type(simple_soup))
 def find_title():
-    print(simple_soup.string('h1'))
     h1_tag = simple_soup.find('h1')
     print(h1_tag.string)
 
 def find_list_items():
    list_items = simple_soup.find_all('li')
+   list_contents = [e.string for e in list_items]
    print(list_items)
+   print(list_contents)
 
+def find_subtitle():
+    paragraph = simple_soup.find('p', {'class': 'subtitle'})
+    print(paragraph.string)
+
+def find_other_paragraph():
+
+    paragraphs = simple_soup.find_all('p')
+    other_paragraph = [p for p in paragraphs if 'subtitle' not in p.attrs.get('class', [])]
+    print(other_paragraph[0].string)
+    
+    
+    
 find_title()
 find_list_items()
+find_subtitle()
+find_other_paragraph()
