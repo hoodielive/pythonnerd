0d0e7eedee345790ace727de691f69c530a1cdfa
hoodielive <hoodielive@gmail.com>
2019-01-24 20:03:31 -0500

    objects and arg passing

---
+++ misx/objects/arg-passing.py
@@ -0,0 +1,8 @@
+def f(d):
+    return d 
+
+c = [6, 10, 16]
+
+e = f(c)
+
+print(c is e)
---
+++ misx/objects/replace.py
@@ -0,0 +1,7 @@
+f = [14, 23, 37]
+
+def replace(g):
+    g = [17, 28, 37]
+    print("g =", g)
+
+replace(f)
