b7663e040bb0067583b77c0b8fa8767652565946
hoodielive <hoodielive@gmail.com>
2019-01-27 08:53:19 -0500

    create menu

--- complete/advanced/scraping_the_web/scraping-books/app.py
+++ complete/advanced/scraping_the_web/scraping-books/app.py
@@ -6,6 +6,3 @@ page_content = requests.get('http://books.toscrape.com').content
 page = AllBooksPage(page_content)
 
 books = page.books # all_books_page - returns book objects
-
-f = [book for book in books]
-print(f)
---
+++ complete/advanced/scraping_the_web/scraping-books/menu.py
@@ -0,0 +1,8 @@
+from app import books 
+
+def print_best_books():
+    best_books = sorted(books, keys=lambda x: x.rating)[:10] # takes book list from app.py and sorts it (sorted by x.rating)
+    f = [book for book in best_books]
+    print(f)
+
+print_best_books()
