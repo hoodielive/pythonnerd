42cfcec0ee7162e41f0fe2580e22846a1bfa4f9b
hoodielive <hoodielive@gmail.com>
2019-01-27 08:55:35 -0500

    create menu ascending

--- complete/advanced/scraping_the_web/scraping-books/menu.py
+++ complete/advanced/scraping_the_web/scraping-books/menu.py
@@ -1,7 +1,7 @@
 from app import books 
 
 def print_best_books():
-    best_books = sorted(books, keys=lambda x: x.rating)[:10] # takes book list from app.py and sorts it (sorted by x.rating)
+    best_books = sorted(books, keys=lambda x: x.rating * - 1)[:10] # takes book list from app.py and sorts it (sorted by x.rating)
     f = [book for book in best_books]
     print(f)
 
