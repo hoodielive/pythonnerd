a9c4c120564e9bd7b33ac1883e3a2d0f35d7ddce
hoodielive <hoodielive@gmail.com>
2019-01-27 10:09:47 -0500

    alright.. sigh

--- complete/advanced/scraping_the_web/scraping-books/menu.py
+++ complete/advanced/scraping_the_web/scraping-books/menu.py
@@ -1,40 +1,59 @@
-from app import books 
+import logging
 
-USER_CHOICE = '''
-- 'b' to look at 5-star books - 'c' to look at the cheapest books
-- 'n' to just get the next available book on the catalog
+from app import books
+
+logger = logging.getLogger('scraping.menu')
+
+
+USER_CHOICE = '''Enter one of the following
+
+- 'b' to look at 5-star books
+- 'c' to look at the cheapest books
+- 'n' to just get the next available book on the page
 - 'q' to exit
 
 Enter your choice: '''
 
-user_choices = {
-        'b': print_best_books, 
-        'c': print_cheapest_books,
-        'n': get_next_book
-}
 
 def print_best_books():
-    best_books = sorted(books, keys=lambda x: x.rating * - 1)[:10] # takes book list from app.py and sorts it (sorted by x.rating)
-    f = [book for book in best_books]
-    print(f)
+    logger.debug('Finding best books by rating...')
+    best_books = sorted(books, key=lambda x: x.rating * -1)[:5]
+    for book in best_books:
+        print(book)
+
 
 def print_cheapest_books():
-    cheapest_books = sorted(books, keys=lambda x: x.rating * - 1)[:10] # takes book list from app.py and sorts it (sorted by x.rating)
-    f = [book for book in cheapest_books]
-    print(f)
+    logger.debug('Finding best books by price...')
+    cheapest_books = sorted(books, key=lambda x: x.price)[:5]
+    for book in cheapest_books:
+        print(book)
+
 
 books_generator = (x for x in books)
 
+
 def get_next_book():
+    logger.debug('Getting next book from generator of all books...')
     print(next(books_generator))
+
+
+user_choices = {
+    'b': print_best_books,
+    'c': print_cheapest_books,
+    'n': get_next_book
+}
+
 
 def menu():
     user_input = input(USER_CHOICE)
     while user_input != 'q':
+        logger.debug('User did not choose to exit program.')
         if user_input in ('b', 'c', 'n'):
             user_choices[user_input]()
         else:
             print('Please choose a valid command.')
         user_input = input(USER_CHOICE)
+    logger.debug('Terminating program...')
+
 
 menu()
