2bd1a3aa6b0216516419ef15b34f435819b024d8
hoodielive <hoodielive@gmail.com>
2019-01-27 09:11:43 -0500

    create user menu

--- complete/advanced/scraping_the_web/scraping-books/menu.py
+++ complete/advanced/scraping_the_web/scraping-books/menu.py
@@ -1,8 +1,38 @@
 from app import books 
 
+USER_CHOICE = '''
+- 'b' to look at 5-star books
+- 'c' to look at the cheapest books
+- 'n' to just get the next available book on the catalog
+- 'q' to exit
+
+Enter your choice: '''
+
+
 def print_best_books():
     best_books = sorted(books, keys=lambda x: x.rating * - 1)[:10] # takes book list from app.py and sorts it (sorted by x.rating)
     f = [book for book in best_books]
     print(f)
 
-print_best_books()
+def print_cheapest_books():
+    cheapest_books = sorted(books, keys=lambda x: x.rating * - 1)[:10] # takes book list from app.py and sorts it (sorted by x.rating)
+    f = [book for book in cheapest_books]
+    print(f)
+
+books_generator = (x for x in books)
+
+def get_next_book():
+    print(next(books_generator))
+
+def menu():
+    while user_input != 'q':
+        if  user_input == 'b': 
+            print_best_books()
+        elif user_input == 'c':
+            print_cheapest_books()
+        elif user_input == 'n':
+            get_next_book()
+        else:
+            print('Please choose a valid selection.')
+        user_input = input(USER_CHOICE)
+menu()
