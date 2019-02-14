516e92b5722b33849969feb2d89677051bfe2066
hoodielive <hoodielive@gmail.com>
2019-02-14 09:53:16 -0500

    stopped

---
+++ dbcb/changing_the_string_repr_instance.py
@@ -0,0 +1,9 @@
+# change the output produced by printing and viewing instances to something more sensible
+
+class Pair:
+    def __init__(self, x , y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+    
