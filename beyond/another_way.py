dc3dc5cfe7bac9d67a44060ceb8bbaea63057325
hoodielive <hoodielive@gmail.com>
2019-02-24 20:47:58 -0500

    more on that

--- beyond/another_way.py
+++ beyond/another_way.py
@@ -1,6 +1,8 @@
 # export PYTHONPATH=not_searched
-
+# http://docs.python.org/3/using/cmdline.html#envvar-PYTHONPATH
+# http://docs.python.org/3/library/sys.html#sys.path
 import sys 
+
 [p for p in sys.path if 'not_searched' in p]
 
 import path_test
