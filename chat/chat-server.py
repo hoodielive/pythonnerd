2c59cdce2ef4856f56703417450f63cdaaf3a044
hoodielive <hoodielive@gmail.com>
2019-12-01 09:08:09 -0500

    eh..

--- chat/chat-server.py
+++ chat/chat-server.py
@@ -21,3 +21,11 @@ def chat_server():
     while 1:
         # get the list sockets which are ready to be read through select
         # 4th arg, time_out = 0 : poll and never block
+        ready_to_read, ready_to_write, in_error = select.select(SOCKET_LIST,[], [], 0)
+
+        for sock in ready_to_read:
+            # a new connection request received
+            if sock == server_socket:
+                sockfd, addr = server_socket.accept()
+                SOCKET_LIST.append(sockfd)
+                print("Client (%s, %s) connected", % addr)
