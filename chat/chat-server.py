8b9bdb4c0a9ea894f28582839e0e2827797666b4
hoodielive <hoodielive@gmail.com>
2019-11-30 21:59:08 -0500

    chat server beginning

---
+++ chat/chat-server.py
@@ -0,0 +1,14 @@
+import sys
+import socket
+import select
+
+HOST = ''
+SOCKET_LIST = []
+RECV_BUFFER = 4096
+PORT = 9009
+
+def chat_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(HOST, PORT)
+    server_socket.listen(10)
