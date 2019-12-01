d220f8f7cd11eaa60ac1558a56a8192471247c07
hoodielive <hoodielive@gmail.com>
2019-12-01 09:04:05 -0500

    working more on chat-server

--- chat/chat-server.py
+++ chat/chat-server.py
@@ -12,3 +12,12 @@ def chat_server():
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind(HOST, PORT)
     server_socket.listen(10)
+
+    # add server socket object to the list of readable connections
+    SOCKET_LIST.append(server_socket)
+
+    print("Chat server started on port " + str(PORT))
+
+    while 1:
+        # get the list sockets which are ready to be read through select
+        # 4th arg, time_out = 0 : poll and never block
